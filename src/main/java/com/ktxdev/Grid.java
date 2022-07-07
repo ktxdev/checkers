71792fc12e71af63d54784c198084f63a9ca8cc7
ktxdev <sean.ktxdev@gmail.com>
2022-07-07 21:30:49 +0200

    Added checkers board items

--- src/main/java/com/ktxdev/Grid.java
+++ src/main/java/com/ktxdev/Grid.java
@@ -2,11 +2,11 @@ package com.ktxdev;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 
 import static com.ktxdev.GameBoard.*;
-import static java.awt.Color.BLACK;
-import static java.awt.Color.WHITE;
+import static java.awt.Color.*;
 
 public class Grid extends JComponent {
     @Override
@@ -24,6 +24,31 @@ public class Grid extends JComponent {
                 }
 
                 graphics2D.fillRect(posX, poxY, GRID_WIDTH, GRID_HEIGHT);
+            }
+        }
+
+        for (int r = 0; r < NO_OF_GRIDS; r++) {
+            for (int c = 0; c < NO_OF_GRIDS; c++) {
+                if (c == 3 || c == 4)
+                    continue;
+
+                if ((r % 2 == 0 && c % 2 == 0) || (r  % 2 == 1 && c  % 2 == 1)) {
+                    int posX = r * GRID_WIDTH;
+                    int poxY = c * GRID_HEIGHT;
+                    var rectangle = new Rectangle2D.Double(posX, poxY, GRID_WIDTH, GRID_HEIGHT);
+
+                    double centerX = rectangle.getCenterX();
+                    double centerY = rectangle.getCenterY();
+                    var circle = new Ellipse2D.Double();
+                    var radius = (GRID_WIDTH - 40) / 2;
+                    circle.setFrameFromCenter(centerX, centerY, centerX + radius, centerY + radius);
+                    if (c < 4) {
+                        graphics2D.setPaint(RED);
+                    } else {
+                        graphics2D.setPaint(GREEN);
+                    }
+                    graphics2D.fill(circle);
+                }
             }
         }
     }
--- src/main/java/com/ktxdev/Main.java
+++ src/main/java/com/ktxdev/Main.java
@@ -4,7 +4,6 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         EventQueue.invokeLater(() -> {
             var gameBoard = new GameBoard();
             gameBoard.setVisible(true);
